dcfa72ec3022dba529c6eda290a0136b0632cdb7
pedro <pedrohperoni@gmail.com>
2022-01-28 16:51:54 -0300

    add interaction with mongodb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,42 @@
 import express, { json } from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(json());
 app.use(cors());
+
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+let db;
+
+mongoClient.connect().then(() => {
+  db = mongoClient.db("API-bate-papo");
+});
+
+app.get("/participants", (req, res) => {
+  db.collection("participants")
+    .find({})
+    .toArray()
+    .then((x) => {
+      res.send(x);
+    });
+});
+
+app.post("/participants", (req, res) => {
+  db.collection("participants")
+    .insertOne(req.body)
+    .then(() => {
+      res.sendStatus(201);
+    });
+});
+
+app.get("/messages", (req, res) => {
+  res.send("OK");
+});
+
+app.post("/messages", (req, res) => {});
+
+app.post("/status", (req, res) => {});
 
 app.listen(5000, () => {
   console.log("Server listening on port", 5000);
